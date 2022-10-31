36323faa6becefd180324ec381d79deb16372710
naoki0811 <naoki.k.0811.0811@gmail.com>
2022-10-31 15:06:02 +0900

    商品情報編集機能　修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :show, :edit, :update]
+  before_action :set_item, only: [:edit, :show, :update]
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
