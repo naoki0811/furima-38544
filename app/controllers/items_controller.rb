8f6f845379cae80867d94c836a6e69bc89239c6f
naoki0811 <naoki.k.0811.0811@gmail.com>
2022-10-31 16:06:04 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,6 +35,13 @@ class ItemsController < ApplicationController
 
   def show
   end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path
+  end
+
  
   def item_params
     params.require(:item).permit(:product_name, :description, :category_id, :situation_id, :shopping_cost_id, :prefecture_id, :shipping_id, :price, :image).merge(user_id: current_user.id)
