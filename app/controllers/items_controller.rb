986eb0e2d2a71a0addb39563f4bcd2e8fcd4d27a
naoki0811 <naoki.k.0811.0811@gmail.com>
2022-10-31 14:48:58 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show, :edit, :update]
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
@@ -18,12 +19,31 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    if @item.user_id != current_user.id
+       redirect_to root_path
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
   def show
-    @item = Item.find(params[:id])
   end
  
   def item_params
     params.require(:item).permit(:product_name, :description, :category_id, :situation_id, :shopping_cost_id, :prefecture_id, :shipping_id, :price, :image).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+
 
 end
